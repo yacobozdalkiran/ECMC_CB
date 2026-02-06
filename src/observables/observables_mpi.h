54e2ea13a87536a58f5b5169db757b929d1cc195
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-02-06 18:59:23 +0100

    Added HaloObs

---
+++ src/observables/observables_mpi.h
@@ -0,0 +1,19 @@
+//
+// Created by ozdalkiran-l on 1/8/26.
+//
+
+#ifndef INC_4D_MPI_OBSERVABLES_H
+#define INC_4D_MPI_OBSERVABLES_H
+
+#include "../gauge/GaugeField.h"
+#include "../mpi/HalosObs.h"
+#include "../mpi/MpiTopology.h"
+
+namespace mpi::haloobs {
+    double sum_plaquette_bulk(const GaugeField &field, const GeometryCB &geo);
+    double sum_plaquette_boundaries(const GaugeField &field, const GeometryCB &geo, const HaloObs &halo_obs);
+    double mean_plaquette_local(const GaugeField &field, const GeometryCB &geo, HaloObs &halo_obs, mpi::MpiTopology &topo);
+    double mean_plaquette_global(const GaugeField &field, const GeometryCB &geo, HaloObs &halo_obs, mpi::MpiTopology &topo);
+}
+
+#endif //INC_4D_MPI_OBSERVABLES_H
